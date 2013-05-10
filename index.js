2248b8b74d3867b47b086a44dba27e1fd7461fa4
ladinu <ladinu@gmail.com>
2013-05-10 08:30:56 -0700

    now passing all tests

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function hashstream(stream, options, callback) {
 
   if (arguments.length < 2) {
     throw new Error('must provide a stream and a callback');
+    return;
   } else if (typeof options === 'function') {
     var callback = options;
     var options  = null;
@@ -19,13 +20,13 @@ function hashstream(stream, options, callback) {
   if (!stream.readable) 
     callback(new Error('stream must be readable'), null);
 
-  var hash = crypto.createHash(options.hash);
+  var hash = crypto.createHash(options.hash || 'sha1');
 
   stream.on('data', function(data) {
     hash.update(data);
   });
   stream.on('end', function() {
-    callback(null, hash.digest(options.digest));
+    callback(null, hash.digest(options.digest || 'hex'));
   });
 
   stream.on('error', callback);
--- test/hashstream.test.js
+++ test/hashstream.test.js
@@ -1,4 +1,5 @@
 var path   = require('path');
+var stream = require('stream');
 var assert = require('assert');
 var fs     = require('fs');
 
@@ -6,8 +7,8 @@ var hashStream = require('../');
 
 var fname  = 'tf0';
 var hashes = {
-    'sha1' : '9f6fd4c282035567e042b54e50c51081bf311abb'
-  , 'md5'  : '78fb701bc318daaef09211b8462addcd'
+    'sha1' : ['9f6fd4c282035567e042b54e50c51081bf311abb', 'n2/UwoIDVWfgQrVOUMUQgb8xGrs=']
+  , 'md5'  : ['78fb701bc318daaef09211b8462addcd']
 }
 
 
@@ -21,27 +22,42 @@ describe('hashstream', function() {
   it('should return a sha1 in hex by default', function(done) {
     hashStream(read(fname), function(err, hash) {
       if (err) return done(err);
-      done(assert.strictEqual(hash, hashes.sha1));
+      done(assert.strictEqual(hash, hashes.sha1[0]));
     });
   });
 
-  it.skip('should throw error when no argument are given', function() {
-
+  it('should throw error when no argument are given', function() {
+    assert.throws(hashStream, Error);
   });
 
-  it.skip('should throw error when no callback is given', function() {
+  it('should throw error when no callback is given', function() {
+    var file = read(fname);
+    var options = {'hash': 'md5'};
 
+    assert.throws(function(){hashStream(file, options)}, Error);
   });
 
-  it.skip('should throw error when stream is not readable', function() {
+  it('should throw error when stream is not readable', function(done) {
+    var strm = new stream();
+    strm.writable = true;
+    strm.readable = false;
 
+    hashStream(strm, function(err, result) {
+      if (err) done(); else done(new Error('was expecting an error'));
+    });
   });
 
-  it.skip('should allow to set digest encoding', function() {
-
+  it('should allow to set digest encoding', function(done) {
+    hashStream(read(fname), {'digest':'base64'}, function(err, hash) {
+      if (err) return done(err);
+      done(assert.strictEqual(hash, hashes.sha1[1]));
+    });
   });
 
-  it.skip('should allow to set hash type', function() {
-
+  it('should allow to set hash type', function(done) {
+    hashStream(read(fname), {'digest': 'hex', 'hash': 'md5'}, function(err, hash) {
+      if (err) return done(err);
+      done(assert.strictEqual(hash, hashes.md5[0]));
+    });
   });
 });
